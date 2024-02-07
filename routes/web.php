759e5fe18bbfbbc36cb1898b020e04c29321f5fe
Walid Saifi <saifiwalid21@gmail.com>
2024-02-07 22:56:12 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ Route::get('/dashebord', function () {
     return view('dashebord');
 });
 
-Route::get('/register',[AuthentificationController::class, 'pageregister']);
+Route::get('/register',[AuthentificationController::class, 'pageregister'])->name('pageregister');
 Route::post('/',[AuthentificationController::class, 'registerSave'])->name('register.save');
 Route::get('/login',[AuthentificationController::class,'pagelogin'])->name('login.page');
+Route::post('/',[AuthentificationController::class,'loginAction'])->name('login.Action');
 
