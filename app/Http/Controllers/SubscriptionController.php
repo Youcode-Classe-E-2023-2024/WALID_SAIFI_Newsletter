20f2d029ec740b58d0a7640fc183ab676b3832ef
Walid Saifi <saifiwalid21@gmail.com>
2024-02-09 11:57:59 +0100

    Update SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscription;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
+
+
+
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $userExists = Subscription::where('email', $request->email)->exists();
+
+        if ($userExists) {
+            return back()->withErrors(['email' => 'This email is already subscribed.']);
+        }
+
+        Subscription::create([
+            'email' => $request->email,
+        ]);
+        return  redirect('home');
+    }
 
 }
