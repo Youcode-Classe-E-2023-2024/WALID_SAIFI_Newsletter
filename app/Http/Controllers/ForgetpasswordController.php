b9201210631eda58b618de7e4565b045d57e811e
Walid Saifi <saifiwalid21@gmail.com>
2024-02-11 20:50:26 +0100

    Create ForgetpasswordController.php

---
+++ app/Http/Controllers/ForgetpasswordController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ForgetpasswordController extends Controller
+{
+   function fogetpassword(){
+        return view('forget_password');
+    }
+}
