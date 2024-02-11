5a9bfa2adb49ab4ff65b504ca2f79a59a0272604
Walid Saifi <saifiwalid21@gmail.com>
2024-02-11 21:28:32 +0100

    Update ForgetpasswordController.php

--- app/Http/Controllers/ForgetpasswordController.php
+++ app/Http/Controllers/ForgetpasswordController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class ForgetpasswordController extends Controller
 {
@@ -11,7 +15,21 @@ class ForgetpasswordController extends Controller
     }
 
     public  function fogetpasswordPost(Request $request){
+     $request->validate([
+         'email' => 'required|email'
+     ]);
+     $token = Str::random(64);
 
+     DB::teble('password_reset_tokens')->insert([
+          'email' => $request->email,
+          'token' => $request->token,
+          'created_at' => Carbon::now()
+     ]);
+     Mail::send('Email.forget_password' , ['token' => $token]);
+
+    }
+
+    public function rest_password(){
 
     }
 }
