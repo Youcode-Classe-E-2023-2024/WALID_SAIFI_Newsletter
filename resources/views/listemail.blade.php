0ef041cb2c97ee7baba18f9222bd4e151685d9d0
Walid Saifi <saifiwalid21@gmail.com>
2024-02-13 10:21:23 +0100

    Create listemail.blade.php

---
+++ resources/views/listemail.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+@section('content')
+
+    <div class="col-md-12 mb-3">
+        <div class="card text-white bg-dark">
+            <div class="card-body">
+                <i class="fas fa-folder fa-2x"></i>
+                <h5 class="card-title">Liste des emails</h5>
+                <div class="table-responsive">
+                    <table class="table table-dark">
+                        <thead>
+                        <tr>
+                            <th scope="col">Email</th>
+                            <th scope="col">Date de création</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($list as $email)
+                            <tr>
+                                <td>{{ $email->email }}</td>
+                                <td>{{ $email->created_at }}</td>
+                                <td>
+                                <td>
+                                    <form action="{{ route('emails.softDelete', ['list' => $list]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">softDelete</button>
+                                    </form>
+                                </td>
+
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
