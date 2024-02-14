b8cab1e95e51760af7e01f8c0942202c325c7a65
Walid Saifi <saifiwalid21@gmail.com>
2024-02-14 15:09:32 +0100

    Update TempletController.php

--- app/Http/Controllers/TempletController.php
+++ app/Http/Controllers/TempletController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TempletController extends Controller
 {
-    
+    public  function  index(){
+        return view('ajouter_templet');
+    }
+    public function add_templet(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'titre' => 'required',
+            'description' => 'required'
+        ]);
+
+
+    }
+
+
+
+
 }
