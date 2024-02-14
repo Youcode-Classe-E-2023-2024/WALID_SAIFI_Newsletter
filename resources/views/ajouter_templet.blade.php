d1f51e67f334bc5ce182d5adc168f1ef23def537
Walid Saifi <saifiwalid21@gmail.com>
2024-02-14 16:55:03 +0100

    Update ajouter_templet.blade.php

--- resources/views/ajouter_templet.blade.php
+++ resources/views/ajouter_templet.blade.php
@@ -8,34 +8,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('ajouter_templet') }}" enctype="multipart/form-data">
+                        @csrf
                         <div class="mb-4">
-                            <label class="text-xl text-gray-600">Title <span class="text-red-500">*</span></label></br>
-                            <input type="text" class="border-2 border-gray-300 p-2 w-full" name="title" id="title" value="" required>
+                            <label class="text-xl text-gray-600">Titre <span class="text-red-500">*</span></label></br>
+                            <input type="text" class="border-2 border-gray-300 p-2 w-full" name="titre" id="titre" value="" required>
                         </div>
 
                         <div class="mb-4">
                             <label class="text-xl text-gray-600">Description</label></br>
-                            <input type="text" class="border-2 border-gray-300 p-2 w-full" name="description" id="description" placeholder="(Optional)">
+                            <input type="text" class="border-2 border-gray-300 p-2 w-full" name="description" id="description" placeholder="(Optionnel)">
                         </div>
 
                         <div class="mb-8">
-                            <label class="text-xl text-gray-600">Content <span class="text-red-500">*</span></label></br>
-                            <textarea name="content" class="border-2 border-gray-500">
-
-                            </textarea>
+                            <label class="text-xl text-gray-600">Contenu <span class="text-red-500">*</span></label></br>
+                            <textarea name="content" class="border-2 border-gray-500"></textarea>
                         </div>
 
                         <div>
                             <label for="formFileLg" class="form-label">Upload d'image, vidéo ou fichier</label>
-                            <input class="form-control form-control-lg" id="formFileLg" type="file">
+                            <input class="form-control form-control-lg" name="media" id="formFileLg" type="file" required>
                         </div>
 
                         <div class="flex p-1">
-
-                            <button type="submit" class="p-3 bg-blue-500 text-white hover:bg-blue-400" required>Submit</button>
+                            <button type="submit" class="p-3 bg-blue-500 text-white hover:bg-blue-400">Soumettre</button>
                         </div>
                     </form>
+
+                    <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+                    <script>
+                        CKEDITOR.replace('content');
+                    </script>
+
                 </div>
             </div>
         </div>
