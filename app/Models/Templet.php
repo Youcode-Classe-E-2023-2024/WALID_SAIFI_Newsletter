040079750207bd7acb5defa1d8773185dfd77a87
Walid Saifi <saifiwalid21@gmail.com>
2024-02-15 20:40:24 +0100

    Update Templet.php

--- app/Models/Templet.php
+++ app/Models/Templet.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use App\Models\Media;
 
 class Templet extends Model implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'Media');
+    }
 
     protected $fillable = [
          'titre',
@@ -18,9 +25,4 @@ class Templet extends Model implements HasMedia
          'content',
          'user_id'
     ];
-
-
-
-    
-
 }
