a47a90045da38121857020d528cecf40a413615c
Walid Saifi <saifiwalid21@gmail.com>
2024-02-15 11:18:18 +0100

    Update liste_tmp.blade.php

--- resources/views/liste_tmp.blade.php
+++ resources/views/liste_tmp.blade.php
@@ -6,7 +6,7 @@
 
 
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-
+                @foreach($templates as $tmp)
                 <div class="col mb-4">
                     <div class="card shadow-sm">
                         <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -15,6 +15,7 @@
                             <text x="50%" y="50%" fill="#eceeef" dy=".3em">frrrrrffr</text>
                         </svg>
                         <div class="card-body">
+                         <div>titre:{{$tmp->titre}}</div>
 
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
@@ -50,7 +51,7 @@
                         </div>
                     </div>
                 </div>
-
+                @endforeach
             </div>
         </div>
     </div>
