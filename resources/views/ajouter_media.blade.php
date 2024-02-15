c955b454b19dfd445be58f849b5e68b19b96c393
Walid Saifi <saifiwalid21@gmail.com>
2024-02-15 20:40:43 +0100

    Update ajouter_media.blade.php

--- resources/views/ajouter_media.blade.php
+++ resources/views/ajouter_media.blade.php
@@ -1,40 +1,49 @@
 @extends('layout')
 @section('content')
-
-
-    <div class="container mt-5">
-        <div class="container mt-5">
-            <div class="card bg-dark text-white col-md-7">
-                <div class="card-body ">
-                    <h5 class="card-title">Ajouter Templet</h5>
-                    <form>
-
-
-                        <div class="mb-3">
-                            <label for="inputAddress" class="form-label">Titre</label>
-                            <input type="text" class="form-control" name="titre" placeholder="Titre">
-                        </div>
-                        @error('titre')
-                        <span class="text-danger small">{{ $message }}</span>
-                        @enderror
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label for="inputCity" class="form-label">Descrition</label>
-                                <textarea class="form-control" name="description" rows="3"></textarea>
-                            </div>
-                            @error('description')
-                            <span class="text-danger small">{{ $message }}</span>
-                            @enderror
-                            <div>
-                                <label for="formFileLg" class="form-label">Upload d'image, vidéo ou fichier</label>
-                                <input class="form-control form-control-lg" id="formFileLg" type="file">
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">ajouter</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+@if(session('success'))
+    <div class="alert alert-success">{{session('success')}}</div>
+@endif
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+@endif
+
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better Data</span> Ajouter media</h1>
+
+
+  <!--  <form action="{{route('media')}}" method="post">
+    @csrf
+    <div class="flex items-center justify-center w-full">
+        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                </svg>
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            </div>
+            <input id="dropzone-file" type="file" class="hidden" name="media" />
+        </label>
+    </div>
+        <div class="flex justify-center">
+            <button type="submit" class="text-white bg-gradient-to-br from-purple-700 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 my-6 hover:mb-2 hover:bg-purple-400">Submit Media</button>
+        </div>
+</form>-->
+
+
+<form action="{{ route('media') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    <div>
+        <label for="formFileLg" class="form-label">Large file input example</label>
+        <input class="form-control form-control-lg"  name ="media" type="file">
+    </div>
+    <button type="submit" class="text-white bg-gradient-to-br from-purple-700 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 my-6 hover:mb-2 hover:bg-purple-400">Submit Media</button>
+</form>
+
 
 @endsection
