a5797390774657cf97b18c0774505f25ff35a706
Walid Saifi <saifiwalid21@gmail.com>
2024-02-16 23:37:51 +0100

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,65 +1,19 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 
-class RoleSeeder extends Seeder
+class roleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        // Création des permissions
-        $permissionSuppressionEmail = Permission::firstOrCreate(['name' => 'supprimer email']);
-        $permissionModifierUtilisateur = Permission::firstOrCreate(['name' => 'modifier utilisateur']);
-        $permissionSupprimerUtilisateur = Permission::firstOrCreate(['name' => 'supprimer utilisateur']);
-        $permissionCreationTemplate = Permission::firstOrCreate(['name' => 'créer template']);
-        $permissionEnvoyerTemplate = Permission::firstOrCreate(['name' => 'envoyer template']);
-        $permissionModifierTemplate = Permission::firstOrCreate(['name' => 'modifier template']);
-        $permissionGenererPDF = Permission::firstOrCreate(['name' => 'générer pdf']);
 
-        // Création des rôles
-        $roleRedacteur = Role::firstOrCreate(['name' => 'rédacteur']);
-        $roleAdmin = Role::firstOrCreate(['name' => 'admin']);
-        $rolevieux = Role::firstOrCreate(['name' => 'viewer']);
-
-        // Assignation des permissions aux rôles
-        $this->assignPermissionsToRole($roleRedacteur, [
-            $permissionModifierUtilisateur,
-            $permissionSupprimerUtilisateur,
-            $permissionCreationTemplate,
-            $permissionEnvoyerTemplate,
-            $permissionModifierTemplate,
-            $permissionGenererPDF
-        ]);
-
-        $this->assignPermissionsToRole($roleAdmin, [
-            $permissionSuppressionEmail,
-            $permissionModifierUtilisateur,
-            $permissionSupprimerUtilisateur,
-            $permissionCreationTemplate,
-            $permissionEnvoyerTemplate,
-            $permissionModifierTemplate,
-            $permissionGenererPDF
-        ]);
-    }
-
-    /**
-     * Assigner des permissions à un rôle s'ils ne sont pas déjà assignées.
-     *
-     * @param \App\Models\Role $role
-     * @param array $permissions
-     * @return void
-     */
-    private function assignPermissionsToRole(Role $role, array $permissions): void
-    {
-        foreach ($permissions as $permission) {
-            if (!$role->hasPermissionTo($permission)) {
-                $role->givePermissionTo($permission);
-            }
-        }
+           Role::create(['name' => 'rédacteur' , 'guard_name' => 'web']);
+           Role::create(['name' => 'admin' , 'guard_name' => 'web']);
+           Role::create(['name' => 'viewer' , 'guard_name' => 'web']);
     }
 }
