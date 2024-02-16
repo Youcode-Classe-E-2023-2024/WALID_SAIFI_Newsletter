f53afcda9f81535a1caf17791d304574e7c8aacf
Walid Saifi <saifiwalid21@gmail.com>
2024-02-16 15:22:30 +0100

    Update TempletController.php

--- app/Http/Controllers/TempletController.php
+++ app/Http/Controllers/TempletController.php
@@ -6,23 +6,25 @@ namespace App\Http\Controllers;
 use App\Models\Media;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Templet;
 
 class TempletController extends Controller
 {
 
-
     public function index()
     {
-        $medias = Media::all();
+        $medias = Media::orderBy('created_at','desc')->get();
+        // dd($medias);
+
         return view('ajouter_templet',['medias' => $medias]);
     }
 
 
     public function store(Request $request)
     {
-        // Validation des données
+
         $validated = $request->validate([
             'titre' => 'required|min:5',
             'description' => 'nullable|min:5',
@@ -30,7 +32,7 @@ class TempletController extends Controller
             'selected_media' => 'nullable|array', // Assurez-vous que c'est un tableau
         ]);
 
-        // Création du modèle
+
         $template = Templet::create([
             'titre' => $validated['titre'],
             'description' => $validated['description'],
@@ -38,12 +40,12 @@ class TempletController extends Controller
             'user_id' => auth()->id(),
         ]);
 
-        // Associer les médias sélectionnés au modèle
+
         if (isset($validated['selected_media'])) {
             $template->syncMedia($validated['selected_media']);
         }
 
-        // Redirection
+
         return redirect()->route('ajouter');
     }
 
