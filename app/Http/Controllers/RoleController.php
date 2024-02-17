ddcc7618fb6815569e5efdd3f82a6a87da8a4269
Walid Saifi <saifiwalid21@gmail.com>
2024-02-18 00:37:04 +0100

    Update RoleController.php

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RoleController extends Controller
 {
@@ -11,4 +12,21 @@ class RoleController extends Controller
         $roles =Role::all();
          return view('role',compact('roles'));
      }
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+        ]);
+
+
+      //  Role::create($validated);
+        Role::create([
+            'name' => $validated['name'],
+            'guard_name' => 'web', // Remplacez 'web' par le nom du gardien utilisé dans votre application
+        ]);
+
+        return redirect(route('role'))->with('success', 'Le rôle a été ajouté avec succès.');
+    }
+
 }
