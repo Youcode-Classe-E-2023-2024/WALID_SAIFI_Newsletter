b0234beb73909038d59edb7eed8c957b3b30acb7
Walid Saifi <saifiwalid21@gmail.com>
2024-02-17 23:57:47 +0100

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -55,12 +55,27 @@
 
 <div class="container-fluid">
     <div class="row  w-35 p-5">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse pl-3 h-150">
+
+
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse pl-3 h-300">
 
             <!-- Contenu de la barre latérale -->
             <div class="position-sticky pt-3">
                 <ul class="nav flex-column">
 
+                  @role('Admin')
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Gestion des Rôles
+                        </a>
+                               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('role.list')}}">list des Role</a>
+                            <a class="dropdown-item" href="{{route('permission.list')}}">list des Peremition</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    @endrole
                     <!-- Element de navigation : Liste des Utilisateurs -->
                     <li class="nav-item mb-2">
                         <a class="nav-link" href="#">
@@ -101,6 +116,7 @@
                         </a>
                     </li>
 
+
                 </ul>
             </div>
         </nav>
@@ -109,6 +125,14 @@
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Dashboard</h1>
+                @role('Admin')
+                <h1 class="h2">Admin</h1>
+                @endrole
+                @role('consultant')
+                <h1 class="h2">Consultant</h1>
+                @endrole
+
+
                 <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group me-2">
                     </div>
@@ -133,9 +157,10 @@
 
 
         </main>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
 </body>
+
 </html>
