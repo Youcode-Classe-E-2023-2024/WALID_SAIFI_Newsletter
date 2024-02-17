6a320d2848b5786d582750e025c8a1023b850020
Walid Saifi <saifiwalid21@gmail.com>
2024-02-17 23:57:27 +0100

    Update 2024_02_07_161611_create_user_table.php

--- database/migrations/2024_02_07_161611_create_user_table.php
+++ database/migrations/2024_02_07_161611_create_user_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('consultant');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->integer('idRole');
             $table->timestamps();
         });
     }
