ed01f5a127bffb2b9a6ef92d112b996a12edda6e
Walid Saifi <saifiwalid21@gmail.com>
2024-02-18 23:50:01 +0100

    Update PermissionController.php

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -10,8 +10,50 @@ class PermissionController extends Controller
 
 
     public  function  index(){
-        $permissions = Permission::all();
-        return view('permission',compact('permissions'));
+        $permission = Permission::all();
+
+        return  view('/permission',['permissions' => $permission]);
 
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate(['name' => 'required']);
+
+        $existingPermission = Permission::where('name', $validated['name'])->first();
+
+        if ($existingPermission) {
+            return back()->with('error', 'La permission existe déjà.');
+        }
+
+        Permission::create($validated);
+
+        return redirect(route('permission.list'));
+    }
+
+    public function edit(Permission $permission)
+    {
+       return view('editpermission', ['permission' => $permission]);
+    }
+
+    public  function update(Permission $permission,Request $request ){
+        $data = $request->validate([
+            'name' => 'required ',
+        ]);
+        $existingPermission = Permission::where('name', $data['name'])->first();
+
+        if ($existingPermission) {
+            return back()->with('error', 'La permission existe déjà.');
+        }
+        $permission->update($data);
+
+        return redirect(route('permission.list'));
+    }
+    public  function  destroy(Permission $permission){
+
+            $permission->delete();
+
+            return redirect(route('permission.list'));
+    }
+
 }
